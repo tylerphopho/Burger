3e32159a96cd9b4864b7d8ab63b91ff397a8551a
Tyler Pho <owner@Tyler-Phos-MacBook-Pro.local>
2020-03-30 14:34:32 -0600

    created orm.js file with functions

--- config/orm.js
+++ config/orm.js
@@ -1,6 +1,37 @@
 const connection = require("./connection");
 
+let orm = {
+    selectAll: function(cb) {
+        connection.query("SELECT * FROM burgers", function(err, res){
+            if (err) throw err;
+            cb(res);
+        });
+    },
 
-selectAll();
-insertOne();
-updateOne();
+    insertOne: function(burger, cb) {
+        connection.query("INSERT INTO burgers (burgers_name) VALUES (?)",
+        [burger],
+        (err, resp) => {
+            if (err) throw err;
+            cb(resp)
+        }
+    );
+    },
+    updateOne: function(id, cb) {
+        connection.query("UPDATE burgers SET devoured 1 WHERE id = ?",
+        [id],
+        (err, resp) => {
+            if (err) throw err;
+            cb(resp);
+        }
+        );
+    },
+    deleteOne: function(id, cb) {
+        connection.query("DELETE FROM burgers WHERE id = ?", [id], (err, resp) => {
+            if (err) throw err;
+            cb(resp);
+        });
+    }
+};
+
+module.exports = orm;
