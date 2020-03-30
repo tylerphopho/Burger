13aed3c9560e577e82775b86142c819319b84171
Tyler Pho <owner@Tyler-Phos-MacBook-Pro.local>
2020-03-30 16:28:19 -0600

    setup server.js file

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+
+const PORT = process.env.PORT || 8080;
+
+const app  = express();
+
+app.use(express.urlencoded({ extended:true }))
+app.use(express.json());
+
+const exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+var routes = require("./controllers/burgers_controllers");
+
+app.use(routes);
+
+app.listen(PORT, function(){
+    console.log("Server listening on: http://localhost:" + PORT);
+});
